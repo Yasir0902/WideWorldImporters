a7dfd02ca1af26b4de54f8c25f4418917ca3fdc4
Yasir0902 <mohammed.yasir0902@gmail.com>
2025-02-10 15:12:09 +0530

    updates (#6)

--- sales_app/src/main/java/com/example/sales/controllers/StockItemsController.java
+++ sales_app/src/main/java/com/example/sales/controllers/StockItemsController.java
@@ -1,5 +1,6 @@
 package com.example.sales.controllers;
 
+import com.example.sales.dto.StockItemsDTO;
 import com.example.sales.entities.StockItems;
 import com.example.sales.services.StockItemsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +22,19 @@ public class StockItemsController {
     private StockItemsService stockItemsService;
 
     @GetMapping("/all")
-    public List<StockItems> getAllStocks(
+    public List<StockItemsDTO> getAllStocks(
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size
-    ){
-        Pageable pageable = PageRequest.of(page, size,Sort.by("stockItemId").ascending());
-        return stockItemsService.getAllStockItems(pageable);
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(required = false) Long id,
+            @RequestParam(required = false, defaultValue = "false") Boolean invoiceLines,
+            @RequestParam(required = false, defaultValue = "false") Boolean invoices,
+            @RequestParam(required = false, defaultValue = "false") Boolean customers
+    ) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("stockItemId").ascending());
+        if (id == null) {
+            return stockItemsService.getAllStockItems(pageable, invoiceLines, invoices, customers);
+        } else {
+            return stockItemsService.getAllStockItemsById(pageable, id, invoiceLines, invoices, customers);
+        }
     }
 }
